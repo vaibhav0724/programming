28e0a74bfd23173544371614de3cdcd9729fa065
vaibhav0724 <63444851+vaibhav0724@users.noreply.github.com>
2020-04-10 13:28:31 +0530

    Update Swap.c
    Swapping

--- Swap.c
+++ Swap.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int a,b;
+clrscr();
+printf("enter the value of a and b");
+Scanf("%d%d",&a,&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printed("after swapping the value of a&b is %d %d",a,b);
+getch();
+}
